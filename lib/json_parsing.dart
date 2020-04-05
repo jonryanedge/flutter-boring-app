cc14b5cc08db13debf6d66f4892ceffdbce97ba7
jonryanedge <edgeworthjr@gmail.com>
2020-04-05 10:08:14 -0400

    Updated tests

--- lib/json_parsing.dart
+++ lib/json_parsing.dart
@@ -1,15 +1,15 @@
 import 'package:boring_app/src/articles.dart';
 import 'dart:convert';
-import 'package:built_value/built_value.dart';
-import 'package:built_collection/built_collection.dart';
-import 'serializers.dart';
+//import 'package:built_value/built_value.dart';
+//import 'package:built_collection/built_collection.dart';
+//import 'serializers.dart';
+//
+//part 'json_parsing.g.dart';
+//
+//abstract class Article implements Built<Article ArticleBuilder>{
+//  int get id;
+//}
 
-part 'json_parsing.g.dart';
-
-abstrack class Article implements Built<Article ArticleBuilder>{
-  int get id;
-
-}
 List<int> parseTopStories(String json) {
   final parsed = jsonDecode(json);
   final listOfIds = List<int>.from(parsed);
